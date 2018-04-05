621f20c61d15e189b835e5fa781ffc93bcb0ed08
Matt Greyo <matthew.greyo@sky.uk>
2018-04-05 13:35:24 +0100

    Reverted change as per review

--- aws-etcd/start-etcd.sh
+++ aws-etcd/start-etcd.sh
@@ -1,10 +1,6 @@
 #!/bin/sh -e
 
-if [ -e /etc/credentials/vmware_ro_credentials.txt ]; then
-    source /etc/credentials/vmware_ro_credentials.txt
-fi
-
-/etcd-bootstrap -o /etcd-bootstrap.conf $(eval echo ${ETCD_BOOTSTRAP_FLAGS})
+/etcd-bootstrap -o /etcd-bootstrap.conf $ETCD_BOOTSTRAP_FLAGS
 set -a
 source /etcd-bootstrap.conf
 set +a
